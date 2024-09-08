3f14dba4e4d6193ca057ad8f629c8c43c4caf342
tempura327 <cks6658324@yahoo.com.tw>
2024-09-08 16:54:15 +0800

    fix: changed the way to import package to

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // const core = require('@actions/core');
 // const github = require('@actions/github');
+import core from '@actions/core';
+import github from '@actions/github';
 
-// try {
-//   const nameToGreet = core.getInput('name');
-//   console.log(`Hello ${nameToGreet}!`);
+try {
+  console.log('start to run main.js');
 
-//   // 透過github context可以取得觸發workflow的相關資訊
-//   const repoName = github.context.payload.repository;
-//   core.setOutput(`reply', 'It\'s amazing. I installed actions/toolkit to my repo. With the package, I can write a workflow with JavaScript in ${repoName}.`);
+  const nameToGreet = core.getInput('name');
+  console.log(`Hello ${nameToGreet}!`);
+
+  // 透過github context可以取得觸發workflow的相關資訊
+  const repoName = github.context.payload.repository;
+  core.setOutput(`reply', 'It\'s amazing. I installed actions/toolkit to my repo. With the package, I can write a workflow with JavaScript in ${repoName}.`);
   
-// } catch (error) {
-//   // 印出error，並拋出錯誤讓整個workflow失敗
-//   core.setFailed(error.message, 1);
-// }
-
-console.log(1233)
+} catch (error) {
+  // 印出error，並拋出錯誤讓整個workflow失敗
+  core.setFailed(error.message, 1);
+}
