e6f73b0f35aa4790d3b34a36745416d9328ea41b
tempura327 <cks6658324@yahoo.com.tw>
2024-09-08 17:29:57 +0800

    fix: try to fix output

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ try {
 
   // 透過github context可以取得觸發workflow的相關資訊
   const repoName = github.context.payload.repository;
-  core.setOutput(`reply', 'It\'s amazing. I installed actions/toolkit to my repo. With the package, I can write a workflow with JavaScript in ${repoName}.`);
+  core.setOutput(`output1', 'I can write a workflow with JavaScript in ${repoName}.`);
   
 } catch (error) {
   // 印出error，並拋出錯誤讓整個workflow失敗
