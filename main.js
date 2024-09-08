180cce609019a4eeb2090b786c693db76bc2e721
tempura327 <cks6658324@yahoo.com.tw>
2024-09-08 16:52:23 +0800

    chore: give a try

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
-try {
-  const nameToGreet = core.getInput('name');
-  console.log(`Hello ${nameToGreet}!`);
+// try {
+//   const nameToGreet = core.getInput('name');
+//   console.log(`Hello ${nameToGreet}!`);
 
-  // 透過github context可以取得觸發workflow的相關資訊
-  const repoName = github.context.payload.repository;
-  core.setOutput(`reply', 'It\'s amazing. I installed actions/toolkit to my repo. With the package, I can write a workflow with JavaScript in ${repoName}.`);
+//   // 透過github context可以取得觸發workflow的相關資訊
+//   const repoName = github.context.payload.repository;
+//   core.setOutput(`reply', 'It\'s amazing. I installed actions/toolkit to my repo. With the package, I can write a workflow with JavaScript in ${repoName}.`);
   
-} catch (error) {
-  // 印出error，並拋出錯誤讓整個workflow失敗
-  core.setFailed(error.message, 1);
-}
+// } catch (error) {
+//   // 印出error，並拋出錯誤讓整個workflow失敗
+//   core.setFailed(error.message, 1);
+// }
+
+console.log(1233)
