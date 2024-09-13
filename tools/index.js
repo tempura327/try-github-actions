cbaf5df229981bd96e27959cbf94b38275c5c182
tempura327 <cks6658324@yahoo.com.tw>
2024-09-13 20:56:43 +0800

    feat: save content from index.js to env

--- tools/index.js
+++ tools/index.js
@@ -6,4 +6,4 @@ const userName = argv.name;
 
 console.log(`Hello ${userName}. How's it going with your new workflow?`);
 
-console.log('echo"reply=It\'s good. I\'m try to run JS file in it." >> GITHUB_OUTPUT')
+console.log('echo"reply=It\'s good. I\'m try to run JS file in it." >> $GITHUB_OUTPUT')
