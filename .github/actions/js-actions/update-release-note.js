f7e108352d052784e0fb3b4abf6cbf99eb844a6f
tempura327 <cks6658324@yahoo.com.tw>
2024-09-22 22:29:27 +0800

    chore: modified update-release-note.js

--- .github/actions/js-actions/update-release-note.js
+++ .github/actions/js-actions/update-release-note.js
@@ -7,7 +7,7 @@ const releaseDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate(
 const versionId = core.getInput('versionId');
 const projectId = core.getInput('projectId');
 const jiraToken = core.getInput('jiraToken');
-const jiraDomain = core.getInput('jiraDomain')
+const jiraDomain = core.getInput('jiraDomain');
 
 const bodyData = `{
   "archived": true,
@@ -22,7 +22,7 @@ fetch(`https://${jiraDomain}.atlassian.net/rest/api/3/version/${versionId}`, {
   method: 'PUT',
   headers: {
     'Authorization': `Basic ${Buffer.from(
-      `${jiraToken}`
+      jiraToken
     ).toString('base64')}`,
     'Accept': 'application/json',
     'Content-Type': 'application/json'
