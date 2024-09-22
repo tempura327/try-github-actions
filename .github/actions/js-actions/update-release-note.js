1ab4e52593e7431db2da50f29da9ebe3cdfe0b18
tempura327 <cks6658324@yahoo.com.tw>
2024-09-22 21:57:09 +0800

    feat: added update-release-note.js

---
+++ .github/actions/js-actions/update-release-note.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+const core = require('@actions/core');
+
+const today = new Date();
+const releaseDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+
+const versionId = core.getInput('versionId');
+const projectId = core.getInput('projectId');
+const jiraToken = core.getInput('jiraToken');
+const jiraDomain = core.getInput('jiraDomain')
+
+const bodyData = `{
+  "archived": true,
+  "id": "${versionId}",
+  "projectId": ${projectId},
+  "releaseDate": "${releaseDate}",
+  "released": true,
+  "userReleaseDate": "${releaseDate}"
+}`;
+
+fetch(`https://${jiraDomain}.atlassian.net/rest/api/3/version/${versionId}`, {
+  method: 'PUT',
+  headers: {
+    'Authorization': `Basic ${Buffer.from(
+      `${jiraToken}`
+    ).toString('base64')}`,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: bodyData
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    return response.text();
+  })
+  .then(text => console.log(text))
+  .catch(err => console.error(err));
