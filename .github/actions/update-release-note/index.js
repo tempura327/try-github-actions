738da51723999123354951ed940304b794911d9a
tempura327 <cks6658324@yahoo.com.tw>
2024-09-23 23:22:01 +0800

    chore: recovered workflow_run in release-jira-release-note.yml

--- .github/actions/update-release-note/index.js
+++ .github/actions/update-release-note/index.js
@@ -11,13 +11,14 @@ const jiraToken = core.getInput('jiraToken');
 const jiraDomain = core.getInput('jiraDomain');
 
 // https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-project-versions/#api-rest-api-3-version-id-put
+// 不可以有尾懸掛的,，會報錯"There was an error parsing JSON. Check that your request body is valid."
 const bodyData = `{
-  "archived": true,
   "id": "${versionId}",
   "projectId": "${projectId}",
   "releaseDate": "${releaseDate}",
   "released": true
 }`;
+// "archived": true, 會被丟到封存區，視情況使用
 
 console.log(bodyData)
 
